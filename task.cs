09beafa3c2da415b3ac227bc9064396f8f332816
Дмитрий Лёвкин <118073863+KockAnigilator@users.noreply.github.com>
2024-02-22 21:14:03 +0300

    Добавьте файлы проекта.

---
+++ task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace node1
+{
+    public class task
+    {
+        private string name;
+        private int id;
+        public List<tasklist> List;
+
+        public string Name { get => name; set => name = value; }
+        public int Id { get => id; set => id = value; }
+        
+        public task Create()
+        {
+
+            
+        }
+    }
+}
---
+++ tasklist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace node1
+{
+    public class tasklist
+    {
+        private string name;
+        private int id;
+        private string description;
+
+        public string Name { get => name; set => name = value; }
+        public int Id { get => id; set => id = value; }
+        public string Description { get => description; set => description = value; }
+    }
+}
