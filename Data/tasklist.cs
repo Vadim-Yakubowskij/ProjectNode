3643cbf6d2fb7b7f29bc5651301d04f8172e3e91
Вадим Якубовский <wadimaukubowskij@gmail.com>
2024-03-03 12:41:44 +0300

    Не украл а адаптировал
    ну а че

--- Data/tasklist.cs
+++ Data/tasklist.cs
@@ -6,23 +6,400 @@ using System.Threading.Tasks;
 
 namespace node1
 {
+    /// <summary>
+    /// Статус сортировки
+    /// </summary>
+    public enum SortOrder
+    {
+        /// <summary>
+        /// Данные обновлены или только что инициализированы
+        /// Необходимо провести сортировку
+        /// </summary>
+        Initial,
+
+        /// <summary>
+        /// В произвольном формате (по возрастанию идентификаторов)
+        /// </summary>
+        Unsorted,
+
+        /// <summary>
+        /// По возрастанию
+        /// </summary>
+        Ascending,
+
+        /// <summary>
+        /// По убыванию
+        /// </summary>
+        Descending
+    }
+
+    /// <summary>
+    /// Контейнер данных списка карт для сериализации
+    /// </summary>
+    public class TasklistData : UniqueData
+    {
+        public bool IsEssential;
+        public List<TaskData> List;
+    }
+
+    /// <summary>
+    /// Список карт
+    /// </summary>
     public class Tasklist : Unique<Tasklist>, IUniqueIdentifiable
     {
-        public List<Task> list;
-
-        public Tasklist(string Name) : base(Name)
+        /// <summary>
+        /// Создать объект из контейнера с данными
+        /// </summary>
+        /// <param name="data">Контейнер с данными</param>
+        /// <returns>Объект</returns>
+        public static Tasklist FromData(TasklistData data)
         {
-            List<Task> List = new List<Task>();
-            list = List;
+            List<Task> list = new List<Task>();
+
+            foreach (TaskData cardData in data.List)
+            {
+                list.Add(Task.FromData(cardData));
+            }
+
+            return new Tasklist(data.Id, data.Name, data.IsEssential, list);
         }
-        public void Add(Task element)
+
+        /// <summary>
+        /// Статус закрепления
+        /// </summary>
+        private bool _isEssential;
+
+        /// <summary>
+        /// Хранилище карт
+        /// </summary>
+        private readonly Dictionary<int, Task> _pool = new Dictionary<int, Task>();
+
+        /// <summary>
+        /// Отсортированный по дате обновления
+        /// список идентификаторов карт
+        /// (только невыполненные обычные)
+        /// </summary>
+        private readonly List<int> _idListDefault = new List<int>();
+
+        /// <summary>
+        /// Отсортированный по важности и дате обновления
+        /// список идентификаторов карт
+        /// (только невыполненные важные)
+        /// </summary>
+        private readonly List<int> _idListImportant = new List<int>();
+
+        /// <summary>
+        /// Отсортированный по статусу выполнения и дате обновления
+        /// список идентификаторов карт
+        /// (только выполненные)
+        /// </summary>
+        private readonly List<int> _idListCompleted = new List<int>();
+
+        /// <summary>
+        /// Статус сортировки
+        /// </summary>
+        private SortOrder _status = SortOrder.Initial;
+
+        /// <summary>
+        /// Конструктор списка из заданного списка карт
+        /// </summary>
+        /// <param name="id">Уникальный идентификатор</param>
+        /// <param name="name">Название</param>
+        /// <param name="isEssential">Статус закрепления</param>
+        /// <param name="list">Список карт</param>
+        public Tasklist(int id, string name, bool isEssential, List<Task> list) : this(id, name, isEssential)
         {
-            list.Add(element);
+            foreach (Task task in list)
+            {
+                Set(task);
+            }
         }
 
-        public Tasklist()
+        /// <summary>
+        /// Конструктор пустого списка
+        /// </summary>
+        /// <param name="id">Уникальный идентификатор</param>
+        /// <param name="name">Название</param>
+        /// <param name="isEssential">Статус закрепления</param>
+        /// <exception cref="ArgumentException"></exception>
+        public Tasklist(int id, string name, bool isEssential = false) : base(id, name)
         {
+            IsEssential = isEssential;
+        }
 
+        /// <summary>
+        /// Базовый конструктор с автоматическим определением идентификатора
+        /// </summary>
+        /// <param name="name">Название</param>
+        public Tasklist(string name) : base(name)
+        {
+            // PASS
+        }
+
+        /// <summary>
+        /// Получить контейнер с данными из объекта
+        /// </summary>
+        /// <returns>Контейнер с данными</returns>
+        public TasklistData ToData()
+        {
+            List<TaskData> list = new List<TaskData>();
+
+            foreach (KeyValuePair<int, Task> item in All)
+            {
+                list.Add(item.Value.ToData());
+            }
+
+            return new TasklistData
+            {
+                Id = Id,
+                Name = Name,
+                IsEssential = IsEssential,
+                List = list
+            };
+        }
+
+        /// <summary>
+        /// Доступ к статусу закрепления
+        /// </summary>
+        public bool IsEssential
+        {
+            get => _isEssential;
+            private set => _isEssential = value;
+        }
+
+        /// <summary>
+        /// Доступ к хранилищу карт
+        /// </summary>
+        public Dictionary<int, Task> All => _pool;
+
+        /// <summary>
+        /// Доступ к статусу сортировки
+        /// </summary>
+        public SortOrder Status
+        {
+            get => _status;
+            private set => _status = value;
+        }
+
+        /// <summary>
+        /// Получить карту по идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор карты</param>
+        /// <returns>Объект карты</returns>
+        public Task Get(int id)
+        {
+            return All[id];
+        }
+
+        /// <summary>
+        /// Получить отсортированный
+        /// по дате обновления список карт
+        /// (только невыполненные обычные)
+        /// </summary>
+        /// <param name="order">Статус сортировки</param>
+        /// <returns>Список карт</returns>
+       
+
+        /// <summary>
+        /// Получить отсортированный по важности
+        /// и дате обновления список карт
+        /// (только невыполненные важные)
+        /// </summary>
+        /// <param name="order">Статус сортировки</param>
+        /// <returns>Список карт</returns>
+       
+
+        /// <summary>
+        /// Получить отсортированный по статусу выполнения
+        /// и дате обновления список карт
+        /// (только выполненные)
+        /// </summary>
+        /// <param name="order">Статус сортировки</param>
+        /// <returns>Список карт</returns>
+        
+
+        /// <summary>
+        /// Получить список карт по части названия или описания
+        /// </summary>
+        /// <param name="content">Часть названия или описания карты</param>
+        /// <returns>Список карт</returns>
+        public List<Task> SearchByContent(string content)
+        {
+            content = content.Trim().ToLower();
+
+            List<Task> result = new List<Task>();
+
+            foreach (KeyValuePair<int, Task> item in All)
+            {
+                if (!item.Value.Name.ToLower().Contains(content))
+                {
+                    if (!item.Value.Description.ToLower().Contains(content))
+                    {
+                        continue;
+                    }
+                }
+
+                result.Add(item.Value);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Получить список карт по дате обновления без учета времени
+        /// </summary>
+        /// <param name="date">Дата обновления</param>
+        /// <returns>Список карт</returns>
+        public List<Task> SearchByDateUpdated(DateTime date)
+        {
+            // Поиск карт, обновленных в определенный день указанной даты
+            DateTime min = new DateTime(date.Year, date.Month, date.Day);
+            DateTime max = min.AddHours(23).AddMinutes(59);
+
+            return SearchByDateUpdated(min, max);
+        }
+
+        /// <summary>
+        /// Получить список карт по дате обновления в заданном промежутке
+        /// </summary>
+        /// <param name="minDate">Начало</param>
+        /// <param name="maxDate">Конец</param>
+        /// <returns>Список карт</returns>
+        public List<Task> SearchByDateUpdated(DateTime minDate, DateTime maxDate)
+        {
+            List<Task> result = new List<Task>();
+
+            foreach (KeyValuePair<int, Task> item in All)
+            {
+                if (minDate > item.Value.Date.Updated)
+                {
+                    continue;
+                }
+
+                if (maxDate < item.Value.Date.Updated)
+                {
+                    continue;
+                }
+
+                result.Add(item.Value);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Сохранить карту
+        /// </summary>
+        /// <param name="card">Объект карты</param>
+        public void Set(Task card)
+        {
+            Status = SortOrder.Initial;
+            All[card.Id] = card;
+        }
+
+        /// <summary>
+        /// Удалить карту с заданным идентификатором
+        /// </summary>
+        /// <param name="id">Идентификатор карты</param>
+        /// <returns>Статус успеха удаления</returns>
+        public bool Remove(int id)
+        {
+            Status = SortOrder.Initial;
+
+            return All.Remove(id);
+        }
+
+        /// <summary>
+        /// Проверить, существует ли карта с указанным идентификатором
+        /// </summary>
+        /// <param name="id">Идентификатор</param>
+        /// <returns>Статус проверки</returns>
+        public bool Contains(int id)
+        {
+            return All.ContainsKey(id);
+        }
+
+        /// <summary>
+        /// Получить отсортированный список карт
+        /// </summary>
+        /// <param name="source">Источник</param>
+        /// <param name="order">Статус сортировки</param>
+        /// <returns>Список карт</returns>
+        private List<Task> GetListSorted(List<int> source, SortOrder order = SortOrder.Ascending)
+        {
+            Sort(order);
+
+            List<Task> result = new List<Task>();
+
+            foreach (int id in source)
+            {
+                result.Add(Get(id));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Выбрать подходящий источник для указанного направления сортировки
+        /// </summary>
+        /// <param name="order">Статус сортировки</param>
+        /// <returns>Источник</returns>
+        private IEnumerable<KeyValuePair<int, Task>> Source(SortOrder order)
+        {
+            if (order == SortOrder.Unsorted)
+            {
+                return All;
+            }
+
+            if (order == SortOrder.Ascending)
+            {
+                return All.OrderBy(item => item.Value.Date.Updated ?? item.Value.Date.Created);
+            }
+
+            return All.OrderByDescending(item => item.Value.Date.Updated ?? item.Value.Date.Created);
+        }
+
+        /// <summary>
+        /// Определить идентификатор карты в один из списков для сортировки
+        /// </summary>
+        /// <param name="card">Карта</param>
+        private void Cache(Task task)
+        {
+            _idListDefault.Add(task.Id);
+        }
+
+        /// <summary>
+        /// Отсортировать списки идентификаторов
+        /// </summary>
+        /// <param name="order">Статус сортировки</param>
+        private void Sort(SortOrder order)
+        {
+            // Если списки уже отсортированы в указанном формате
+            if (order == Status)
+            {
+                return;
+            }
+
+            _idListDefault.Clear();
+            _idListImportant.Clear();
+            _idListCompleted.Clear();
+
+            foreach (KeyValuePair<int, Task> item in Source(order))
+            {
+                Cache(item.Value);
+            }
+
+            Status = order;
+        }
+
+        public bool Equals(Tasklist list)
+        {
+            return base.Equals(list) && list.IsEssential == IsEssential;
+        }
+
+        public override string ToString()
+        {
+            return ToData().ToString();
         }
     }
 }
--- Manager.cs
+++ Manager.cs
@@ -8,23 +8,42 @@ using System.Xml.Serialization;
 
 namespace node1
 {
-    class Manager
+    /// <summary>
+    /// Менеджер списков карт и провайдер данных
+    /// Допустимо существование лишь одного экземпляра этого класса
+    /// (зависимость от потока)
+    /// </summary>
+    public sealed class Manager
     {
-        private readonly string _source = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
+        /// <summary>
+        /// Название файла с данными
+        /// </summary>
         public const string FileName = "data.xml";
-        public const string DirectoryName = "Timelon";
-        public string Source => Path.Combine(SourceDirectory, FileName);
-        public string SourceDirectory => Path.Combine(_source, DirectoryName);
 
-        private readonly SortedList<int, Tasklist> _list = new SortedList<int, Tasklist>();
-        public SortedList<int, Tasklist > All => _list;
+        /// <summary>
+        /// Название директории с данными
+        /// </summary>
+        public const string DirectoryName = "Node";
 
+        /// <summary>
+        /// Зарезервированный идентификатор закрепленного списка карт
+        /// </summary>
+        public const int EssentialIdA = 0;
+
+        /// <summary>
+        /// Зарезервированный идентификатор закрепленного списка карт
+        /// </summary>
+        public const int EssentialIdB = 1;
+
+        /// <summary>
+        /// Экземпляр класса одиночки
+        /// </summary>
         private static Manager _instance = null;
-        public void SetList(Tasklist list)
-        {
-            All[list.Id] = list;
-        }
+
+        /// <summary>
+        /// Глобальная точка доступа к классу
+        /// Реализация шаблона Singleton
+        /// </summary>
         public static Manager Instance
         {
             get
@@ -37,19 +56,126 @@ namespace node1
                 return _instance;
             }
         }
+
+        /// <summary>
+        /// Полный путь до директории "Documents"
+        /// </summary>
+        private readonly string _source = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        /// <summary>
+        /// Списки карт
+        /// </summary>
+        private readonly SortedList<int, Tasklist> _list = new SortedList<int, Tasklist>();
+
+        /// <summary>
+        /// Закрепленные списки карт
+        /// </summary>
+        private readonly List<Tasklist> _listEssential = new List<Tasklist>
+        {
+            new Tasklist(EssentialIdA, "Задачи", true),
+        };
+
+        /// <summary>
+        /// Конструктор менеджера
+        /// Не может быть вызван извне
+        /// </summary>
         private Manager()
         {
             LoadData();
         }
-        private void WriteTasklist(List<Tasklist> data)
-        {
-            using (StreamWriter writer = new StreamWriter(Source))
-            {
-                XmlSerializer serializer = new XmlSerializer(typeof(List<Tasklist>));
 
-                serializer.Serialize(writer, data);
+        /// <summary>
+        /// Конструктор менеджера для отладки
+        /// </summary>
+        /// <param name="source">Путь до файла данных</param>
+        /// <exception cref="ArgumentException"></exception>
+        public Manager(string source)
+        {
+            if (source == _source)
+            {
+                throw new ArgumentException("Недопустимо использование источника по-умолчанию для отладки");
             }
+
+            _source = source;
         }
+
+        /// <summary>
+        /// Доступ к файлу с данными
+        /// </summary>
+        public string Source => Path.Combine(SourceDirectory, FileName);
+
+        /// <summary>
+        /// Доступ к директории с данными
+        /// </summary>
+        public string SourceDirectory => Path.Combine(_source, DirectoryName);
+
+        /// <summary>
+        /// Доступ к спискам карт
+        /// </summary>
+        public SortedList<int, Tasklist> All => _list;
+
+        /// <summary>
+        /// Получить список карт по идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор списка карт</param>
+        /// <returns>Список карт</returns>
+        public Tasklist GetList(int id)
+        {
+            return All[id];
+        }
+
+        /// <summary>
+        /// Вставить список карт
+        /// </summary>
+        /// <param name="list">Список карт</param>
+        public void SetList(Tasklist list)
+        {   
+            All[list.Id] = list;
+        }
+
+        /// <summary>
+        /// Удалить список карт по его идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор списка карт</param>
+        /// <returns>Статус успешности удаления</returns>
+        public bool RemoveList(int id)
+        {
+            return All.Remove(id);
+        }
+
+        /// <summary>
+        /// Проверить, существует ли список с указанным идентификатором
+        /// </summary>
+        /// <param name="id">Идентификатор</param>
+        /// <returns>Статус проверки</returns>
+        public bool ContainsList(int id)
+        {
+            return All.ContainsKey(id);
+        }
+
+        /// <summary>
+        /// Сохранить данные
+        /// </summary>
+        public void SaveData()
+        {
+            CreateDataSource();
+
+            // Внедряем закрепленные списки карт в начало
+            InjectEssentials();
+
+            List<TasklistData> data = new List<TasklistData>();
+
+            foreach (KeyValuePair<int, Tasklist> item in All)
+            {
+                data.Add(item.Value.ToData());
+            }
+
+            WriteCardListData(data);
+        }
+
+        /// <summary>
+        /// Создать файл для записи данных
+        /// </summary>
         private void CreateDataSource()
         {
             Directory.CreateDirectory(SourceDirectory);
@@ -59,53 +185,74 @@ namespace node1
                 return;
             }
 
-
+            // Пустой файл
             File.Create(Source).Close();
 
-
-            WriteTasklist(new List<Tasklist>());
+            // Даже если сохранять нечего
+            // В любом случае мы создадим корректную xml основу
+            // И пустой файл превратится в читаемый программой источник
+            WriteCardListData(new List<TasklistData>());
         }
 
+        /// <summary>
+        /// Записать данные в файл
+        /// </summary>
+        /// <param name="data">Данные списков карт</param>
+        private void WriteCardListData(List<TasklistData> data)
+        {
+            using (StreamWriter writer = new StreamWriter(Source))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<TasklistData>));
+
+                serializer.Serialize(writer, data);
+            }
+        }
+
+        /// <summary>
+        /// Загрузить данные
+        /// </summary>
         private void LoadData()
         {
             CreateDataSource();
 
             using (StreamReader reader = new StreamReader(Source))
             {
-                XmlSerializer serializer = new XmlSerializer(typeof(List<Tasklist>));
-                List<Tasklist> data = (List<Tasklist>)serializer.Deserialize(reader);
+                XmlSerializer serializer = new XmlSerializer(typeof(List<TasklistData>));
+                List<TasklistData> data = (List<TasklistData>)serializer.Deserialize(reader);
 
                 // Очищаем списки карт перед загрузкой новых
                 All.Clear();
 
-                foreach (Tasklist item in data)
+                foreach (TasklistData item in data)
                 {
-                    SetList(item);
+                    SetList(Tasklist.FromData(item));
                 }
             }
+
+            // Внедряем закрепленные списки карт в конце
+            // Тем самым ограничиваем возможность их случайной перезаписи
+            InjectEssentials();
         }
 
-        public void SaveData()
+        /// <summary>
+        /// Внедрить закрепленные списки карт
+        /// </summary>
+        private void InjectEssentials()
         {
-            CreateDataSource();
-
-
-            List<Tasklist> data = new List<Tasklist>();
-
-            foreach (KeyValuePair<int, Tasklist> item in All)
+            foreach (Tasklist cardList in _listEssential)
             {
-                data.Add(item.Value);
-            }
+                if (ContainsList(cardList.Id))
+                {
+                    if (GetList(cardList.Id).Equals(cardList))
+                    {
+                        continue;
+                    }
+                }
 
-            WriteCardList(data);
-        }
-        private void WriteCardList(List<Tasklist> data)
-        {
-            using (StreamWriter writer = new StreamWriter(Source))
-            {
-                XmlSerializer serializer = new XmlSerializer(typeof(List<Tasklist>));
-
-                serializer.Serialize(writer, data);
+                // Если под указанным идентификатором уже существует карта
+                // Но ее название отличается от названия закрепленной
+                // Это считается повреждением данных и карта будет перезаписана
+                SetList(cardList);
             }
         }
     }
