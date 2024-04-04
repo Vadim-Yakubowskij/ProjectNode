50d208ed35031e994854eb1f546f325d1cebd69c
Дмитрий Лёвкин <151447813+Vadim-Yakubowskij@users.noreply.github.com>
2024-04-04 19:01:36 +0300

    WIP

--- BD_2/Repository/ClassInterface.cs
+++ BD_2/Repository/ClassInterface.cs
@@ -11,49 +11,44 @@ namespace BD_2
     class ClassInterface : Interface
     {
         private const string ConnectionString = "Data Source=C:\\Users\\1\\Documents\\GitHub\\node1\\BD_2\\BD\\Tasklist.db; FailIfMissing=False";
-        static SQLiteConnection connection;
-        static SQLiteCommand command;
         void create()
         {
             try
             {
-                connection = new SQLiteConnection(ConnectionString);
-                connection.Open();
-                Console.WriteLine("Connected!");
-                command = new SQLiteCommand(connection)
+                string sql = "INSERT INTO \"tasks\" VALUES(NULL, \"сходить с федей в бар\", \"и пк\")";
+                using (SQLiteConnection connection = new SQLiteConnection(ConnectionString))
                 {
-                    CommandText = "INSERT INTO \"tasks\" VALUES(NULL, \"сходить с федей в бар\", \"и пк\")"
-                };
-                Console.WriteLine("Добавлен новый элемент в тасклист:");
-                DataTable data = new DataTable();
-                SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
-                adapter.Fill(data);
+                    connection.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, connection))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
             }
             catch (SQLiteException ex)
             {
-                Console.WriteLine($"Ошибка доступа к базе данных. Исключение: {ex.Message}");
             }
-            Console.Read();
+
         }
         void read()
         {
             try
             {
-                connection = new SQLiteConnection(ConnectionString);
-                connection.Open();
-                Console.WriteLine("Connected!");
-                command = new SQLiteCommand(connection)
+                string sql = "INSERT INTO \"tasks\" VALUES(NULL, \"сходить с федей в бар\", \"и пк\")";
+                using (SQLiteConnection connection = new SQLiteConnection(ConnectionString))
                 {
-                    CommandText = "SELECT * FROM \"tasks\";"
-                };
-                Console.WriteLine("Результат запроса:");
-                DataTable data = new DataTable();
-                SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
-                adapter.Fill(data);
-                Console.WriteLine($"Прочитано {data.Rows.Count} записей из таблицы БД");
-                foreach (DataRow row in data.Rows)
-                {
-                    Console.WriteLine($"id = {row.Field<Int64>("id")} name = {row.Field<string>("name")} more details = {row.Field<string>("more details")}");
+                    connection.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, connection))
+                    {
+                        using (SQLiteDataReader rdr = cmd.ExecuteReader())
+                        {
+                            object id = rdr.GetValue(0);
+                            object name = rdr.GetValue(1);
+                            object more_details = rdr.GetValue(2);
+
+                            Console.WriteLine("{0} \t{1} \t{2}", id, name, more_details);
+                        }
+                    }
                 }
             }
             catch (SQLiteException ex)
@@ -67,7 +62,7 @@ namespace BD_2
             try
             {
 
-                string sql = "INSERT INTO \"tasks\" VALUES(NULL, \"сходить с федей в бар\", \"и пк\")";
+                string sql = "UPDATE \"tasks\"SET name = \"сходить в зал\"WHERE id = 23;";
                 using (SQLiteConnection connection = new SQLiteConnection(ConnectionString))
                 {
                     connection.Open();
