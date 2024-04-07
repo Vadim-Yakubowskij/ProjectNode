85c66d285a14a07204d30cbd75a8df23563a65b4
Дмитрий Лёвкин <151447813+Vadim-Yakubowskij@users.noreply.github.com>
2024-04-07 17:50:38 +0300

    tilt(

--- BD_2/Repository/ClassInterface.cs
+++ BD_2/Repository/ClassInterface.cs
@@ -5,13 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SQLite;
+using System.IO;
+using System.Security.Policy;
+using System.Configuration;
 
 namespace BD_2
 {
     class ClassInterface : Interface
     {
-
-        private const string ConnectionString = "Data Source = C:\\Users\\1\\Documents\\GitHub\\node1\\BD_2\\BD\\Tasklist.db";
+        private string ConnectionString = "Data Source = C:\\Users\\1\\Documents\\GitHub\\node1\\BD_2\\BD\\Tasklist.db";
+        public ClassInterface()
+        {
+            string relativePath = @"BD\Tasklist.db";
+            string parentDir = Path.GetDirectoryName(AppContext.BaseDirectory);
+            string tmp = parentDir.Remove(parentDir.Length - 16, 16);
+            string absolutePath = Path.Combine(tmp, relativePath);
+            ConnectionString = string.Format("Data Source = {0};Version=3; FailIfMissing=False", absolutePath,true);
+        }
+        
         public void create(string name, string more)
         {
             try
@@ -45,9 +56,10 @@ namespace BD_2
                         {
                             while (rdr.Read())
                             {
-                                object id = rdr.GetValue(0);
-                                object name = rdr.GetValue(1);
-                                object more_details = rdr.GetValue(2);
+                                int index = 0;
+                                object id = rdr.GetInt32(index++);
+                                object name = rdr.GetString(index++);
+                                object more_details = rdr.GetString(index++);
 
                                 Console.WriteLine("{0} \t{1} \t{2}", id, name, more_details);
                             }
