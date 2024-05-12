8bdeec8547965fd7563ff144be5a32c59f729bdd
Дмитрий Лёвкин <151447813+Vadim-Yakubowskij@users.noreply.github.com>
2024-05-12 17:48:34 +0300

    potno
    )

--- BD_2/Repository/Task.cs
+++ BD_2/Repository/Task.cs
@@ -1,4 +1,6 @@
-﻿namespace DataBase.Repository
+﻿using System;
+
+namespace DataBase.Repository
 {
     public class Task
     {
@@ -8,6 +10,18 @@
             Date_time = x.Date_time;
             Name = x.Name;
             More_details = x.More_details;
+            if(x.More_details.Length <= 5)
+                More_details_short = x.More_details;
+            else
+            {
+                More_details_short = x.More_details.Substring(0, Math.Min(x.More_details.Length,5)) + "...";
+            }
+            if (x.Name.Length <= 5)
+                Name_short = x.Name;
+            else
+            {
+                Name_short = x.Name.Substring(0, Math.Min(x.Name.Length, 5)) + "...";
+            }
         }
 
         public Task(int id, string date_time, string name, string more_details)
@@ -22,5 +36,7 @@
         public string Date_time { get; set; }
         public string Name { get; set; }
         public string More_details { get; set; }
+        public string More_details_short { get; set; }
+        public string Name_short { get; set; }
     }
 }
