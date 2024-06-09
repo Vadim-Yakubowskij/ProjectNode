dea5f7ac2be10fd3d5621bd13806bdc36b66a9c5
Дмитрий Лёвкин <151447813+Vadim-Yakubowskij@users.noreply.github.com>
2024-06-09 17:40:45 +0300

    обращения к серверу
    /register
    /login

---
+++ Jecub/Migrations/20240609141322_inint.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jecub.Migrations
+{
+    /// <inheritdoc />
+    public partial class inint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Login",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Login",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
---
+++ Jecub/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Jecub
+{
+    public class Rpg
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string RpgClass { get; set; } = string.Empty;
+    }
+}
