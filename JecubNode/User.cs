89c84e4f4a562658f71bf30366a5c85a6da1a733
Вадим Якубовский <wadimaukubowskij@gmail.com>
2024-06-10 22:23:45 +0300

    (
    anluc

---
+++ JecubNode/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JecubNode
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string RpgClass { get; set; } = string.Empty;
+    }
+}
