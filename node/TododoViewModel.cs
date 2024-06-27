64a5be09bb1f1f45abe5d8e787f657c7217f1058
Дмитрий Лёвкин <151447813+Vadim-Yakubowskij@users.noreply.github.com>
2024-06-27 17:44:52 +0300

    ;)

--- node/TododoViewModel.cs
+++ node/TododoViewModel.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
-using node1;
 
 using DataBase.Repository;
 using Task = DataBase.Repository.Task;
